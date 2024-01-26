38cbdaaec47163574456809c156f093cda6b87ad
zhang <1002274599@qq.com>
2024-01-26 22:51:03 +0800

    初始化服务,设置数据库连接

--- src/main/java/com/onus/exercise/ExerciseApplication.java
+++ src/main/java/com/onus/exercise/ExerciseApplication.java
@@ -1,10 +1,12 @@
 package com.onus.exercise;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.onus.exercise.mapper"})
 public class ExerciseApplication {
 
     public static void main(String[] args) {
