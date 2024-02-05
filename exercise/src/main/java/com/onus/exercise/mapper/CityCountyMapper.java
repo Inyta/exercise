4b87f91473fe6046578a97e660eae9b701934d64
zhang <1002274599@qq.com>
2024-02-05 23:19:54 +0800

    删除无效代码

--- exercise/src/main/java/com/onus/exercise/mapper/CityCountyMapper.java
+++
@@ -1,8 +0,0 @@
-package com.onus.exercise.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.onus.exercise.entity.po.CityCounty;
-
-public interface CityCountyMapper extends BaseMapper<CityCounty> {
-
-}
